63c6eb802ce0df0400a51681e58a12059f99df23
Taylor Liss <tliss@camsys.com>
2018-01-30 21:38:03 -0500

    Reduced desktop external area

--- desktop/src/com/tayloraliss/snake/desktop/DesktopLauncher.java
+++ desktop/src/com/tayloraliss/snake/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.tayloraliss.snake.SnakeGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-//		config.width = 1280;
-//		config.height = 960;
+		config.width = 224;
+		config.height = 320;
 		new LwjglApplication(new SnakeGame(), config);
 	}
 }
